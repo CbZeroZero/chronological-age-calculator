c2ada143d6104be3860bd14cd261912f4248fd1e
cbzerozero <cbzerozero@gmail.com>
2024-05-23 11:54:55 +0800

    Refactor JumpToCalculatePage component and update English messages

--- components/JumpToCalculatePage.tsx
+++ components/JumpToCalculatePage.tsx
@@ -1,26 +1,18 @@
-'use client';
-
-import { useRouter } from '@/navigation';
 import { Button } from '@/components/ui/button';
+import Link from 'next/link';
 
 interface ButtonProps {
     text: string;
 }
 
 export default function JumpCalculatePageButton({ text }: ButtonProps) {
-    const router = useRouter();
-
-    const handleClick = () => {
-        router.push("/");
-    };
 
     return <div className='flex justify-center w-full'>
         <Button
-            onClick={handleClick}
             className='my-4'
             variant="emerald"
         >
-            {text}
+            <Link href={"/"} >{text}</Link>
         </Button>
     </div>
 };
