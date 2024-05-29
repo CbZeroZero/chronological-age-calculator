1a06c167274df57b6b10c0dee215b446e81f8a02
cbzerozero <cbzerozero@gmail.com>
2024-05-29 13:04:03 +0800

    Add Clarity

--- app/[local]/layout.tsx
+++ app/[local]/layout.tsx
@@ -3,6 +3,7 @@ import { getMessages } from 'next-intl/server';
 import { Inter } from "next/font/google";
 import Navbar from '@/components/Navbar';
 import { GoogleAnalytics } from '@next/third-parties/google'
+import Script from 'next/script';
 import "../globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -28,6 +29,18 @@ export default async function RootLayout({
         </NextIntlClientProvider>
         {
           process.env.NODE_ENV === 'production' && <GoogleAnalytics gaId="G-XPCMFY2EL5" />
+        }
+        {
+          process.env.NODE_ENV === 'production' &&
+          <Script id="clarity-script" strategy="afterInteractive">
+            {`
+              (function(c,l,a,r,i,t,y){
+                  c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                  t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                  y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+              })(window, document, "clarity", "script", "mjfmss4qax");
+            `}
+          </Script>
         }
       </body>
     </html>
